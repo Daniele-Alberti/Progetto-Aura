d531ac771e34a9aef1601a73d7798d6af845eca4
RipamontiMatteo <matteo.ripamonti@issgreppi.it>
2026-03-04 11:42:02 +0100

    developer: add gui per sensore

--- Attività Pentamestre/main.py
+++ Attività Pentamestre/main.py
@@ -2,8 +2,44 @@ import serial
 import struct
 import datetime as dt
 import json
+import tkinter as tk
 
 arduino = serial.Serial("COM4", 9600)
+
+def LetturaSensore():
+    seriale = arduino.read(32)
+    dati = struct.unpack("2s 4s 4s 2s 4s 16s", seriale)
+
+    id = dati[0].decode()
+    mittente = dati[1].decode()
+    destinatario = dati[2].decode()
+    tipo = dati[3].decode()
+    valoreSensore = dati[4].decode()
+    vuoto = dati[5].decode()
+    
+    labelvalore.configure(text=f"Data lettura: {dt.datetime.now()}\tValore: {valoreSensore}")
+    window.after(100, LetturaSensore)
+
+    html = open("index.html", "w")
+    dato = f"\t\t\t<tr><td>{dt.datetime.now()}</td><td>{valoreSensore}</td></tr>"
+    stringagrossa = intestazioneHtml + dato + chiusuraHtml
+    html.write(stringagrossa)
+    html.close()
+
+    data_json = {"data": str(dt.datetime.now()), "valore": valoreSensore}
+    with open("db.json", "a") as json_file:
+        json_file.write(json.dumps(data_json) + "\n")
+    json_file.close()
+    
+
+window = tk.Tk()
+window.title("Lettura sensore")
+window.geometry("1500x500")
+labelvalore = tk.Label(window, text="Valore: ", font=("Arial", 20))
+labelvalore.grid(column=0, row=1)
+button = tk.Button(window, text="Avvia lettura", command=LetturaSensore)
+button.grid(column=0, row=0)
+
 
 intestazioneHtml = """
 <html>
@@ -26,33 +62,6 @@ chiusuraHtml = """
 
 if __name__ == "__main__":
 
-    while True:
-        seriale = arduino.read(32)
-        dati = struct.unpack("2s 4s 4s 2s 4s 16s", seriale)
-        #print(dati)
-
-        id = dati[0].decode()
-        mittente = dati[1].decode()
-        destinatario = dati[2].decode()
-        tipo = dati[3].decode()
-        valoreSensore = dati[4].decode()
-        vuoto = dati[5].decode()
+    window.mainloop()
         
-        print(f"id: {id}")
-        print(f"mittente: {mittente}")
-        print(f"destinatario: {destinatario}")
-        print(f"tipo: {tipo}")
-        print(f"valore sensore: {valoreSensore}")
-        print(f"vuoto: {vuoto}")
-
-        data_json = {"data": str(dt.datetime.now()), "valore": valoreSensore}
-
-        with open("db.json", "a") as json_file:
-            json_file.write(json.dumps(data_json) + "\n")
-        json_file.close()
-
-        html = open("index.html", "w")
-        dato = f"\t\t\t<tr><td>{dt.datetime.now()}</td><td>{valoreSensore}</td></tr>"
-        stringagrossa = intestazioneHtml + dato + chiusuraHtml
-        html.write(stringagrossa)
-        html.close()
+        
